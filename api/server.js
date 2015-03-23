92aa89ba50cfb2464728ef1d2a63a7d97eb4a2c8
sarah <sarahabimay@gmail.com>
2015-03-23 17:32:36 +0000

    corrected routes and handlers

--- api/server.js
+++ api/server.js
@@ -23,14 +23,14 @@ server.register([Bell, Cookie], function (err) {
 
     server.auth.strategy('github', 'bell', {
         provider: 'github',
-        password: config.secret,
+        password: config.github.password,
         isSecure: false,
-        clientId: config.cKey,
-        clientSecret: config.cSecret
+        clientId: config.github.clientId,
+        clientSecret: config.github.clientSecret
     });
 
     server.auth.strategy('session', 'cookie', {
-        password: config.secret,
+        password: config.cookie.password,
         cookie: 'sid',
         redirectTo: '/login',
         redirectOnTry: false,
