774af251e4326a71277f9fabb4bf217bbcaabd7b
Adam Kowalczyk <adamkowalczyk@gmail.com>
2015-03-24 11:13:41 +0000

    rename param

--- api/routes/routes.js
+++ api/routes/routes.js
@@ -10,7 +10,7 @@ module.exports = [
 	{path: "/admin", 					method: "GET", 		config: controller.admin},
 	{path: "/account/messages", 		method: "GET", 		config: controller.messages},
 	{path: "/admin/member/{memberid}", 	method: "GET",		config: controller.getMember},
-	{path: "/{fileName*}",				method: "GET",		config: controller.serveFile},
+	{path: "/{file*}",					method: "GET",		config: controller.serveFile},
 
 	{path: "/accounts", 				method: "GET", 		config: controller.getAccount},
 	{path: "/accounts", 				method: "POST",  	config: controller.createAccount},
