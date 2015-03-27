6d90879bbd8c686176ef59bb01adf784efc05cd4
Adam Kowalczyk <adamkowalczyk@gmail.com>
2015-03-27 15:40:48 +0000

    test sandbox domain from field

--- api/messages/messages.js
+++ api/messages/messages.js
@@ -72,7 +72,8 @@ var messageTemplates = {
 
 createMessage = function( emailType, data ){
 	var message = {
-					from: 'facmembershipadmin@gmail.com',
+					// from: 'facmembershipadmin@gmail.com',
+					from: 'mail@sandbox987b404359ae4e0ab6199bd4e6ad6d2f.mailgun.org',
 					to: data.email
 				};
 	switch( emailType ){
